2aecea8c61b57e1dd010723017f901c653ab26ef
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2022-10-18 18:17:03 -0400

    unit 6 push
    had to redo my project and start form scratch but it works now

--- Podfile
+++ Podfile
@@ -1,20 +1,22 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'instagram' do
+target 'instagram-clone' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for instagram
   pod 'Parse'
   pod 'AlamofireImage'
+  pod 'MessageInputBar'
 
-  target 'instagramTests' do
+  # Pods for instagram-clone
+
+  target 'instagram-cloneTests' do
     inherit! :search_paths
     # Pods for testing
   end
 
-  target 'instagramUITests' do
+  target 'instagram-cloneUITests' do
     # Pods for testing
   end
 
---
+++ instagram-clone/CameraViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CameraViewController.swift
+//  instagram-clone
+//
+//  Created by Angelo Alies on 10/2/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        
+        //get data from url and add ! to unwrap it
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (sucess, error) in
+            if sucess {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else {
+                print("error!")
+            }
+        }
+    }
+    
+    
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage =  image.af_imageAspectScaled(toFill: size)
+        
+        imageView.image =  scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
